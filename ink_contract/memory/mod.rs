3073160c6e151378526e516528b535346678da97
Paweł <neotheprogramist@proton.me>
2023-09-01 21:57:12 +0200

    basic memory structs

--- ink_contract/memory/mod.rs
+++ ink_contract/memory/mod.rs
@@ -1,1 +1,17 @@
 mod verification_key;
+
+#[derive(Debug)]
+#[ink::storage_item]
+pub struct Point {
+    pub x: [u8; 32],
+    pub y: [u8; 32],
+}
+
+#[derive(Debug)]
+#[ink::storage_item]
+pub struct G2X {
+    pub x0: [u8; 32],
+    pub x1: [u8; 32],
+    pub y0: [u8; 32],
+    pub y1: [u8; 32],
+}
