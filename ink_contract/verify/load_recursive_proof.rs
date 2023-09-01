048c0aa07dface0d680cbc708230dda2073b2535
Bartosz Nowak <bartekn337@gmail.com>
2023-09-01 21:28:08 +0000

    load_recursive_proof todo

--- ink_contract/verify/load_recursive_proof.rs
+++ ink_contract/verify/load_recursive_proof.rs
@@ -1,1 +1,9 @@
-//
+use ethnum::U256;
+
+use crate::ink_contract::BaseUltraVerifier;
+
+pub fn load_recursive_proof(ctx: &mut BaseUltraVerifier) {
+    if U256::from_le_bytes(ctx.verification_key.contains_recursive_proof) > 0 {
+        //TODO
+    }
+}
--- ink_contract/verify/mod.rs
+++ ink_contract/verify/mod.rs
@@ -2,6 +2,7 @@ use crate::ink_contract::BaseUltraVerifier;
 
 pub mod perform_pairing;
 pub mod load_proof;
+pub mod load_recursive_proof;
 
 pub fn verify(ctx: &mut BaseUltraVerifier, _proof: Vec<u8>, _public_inputs: Vec<[u8; 32]>) -> bool {
     todo!()
